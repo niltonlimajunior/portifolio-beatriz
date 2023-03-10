16b712e3a53da639de9fc3b683bd84a1eb31bf10
niltonlimajunior <124934020+niltonlimajunior@users.noreply.github.com>
2023-03-10 19:02:37 -0300

    att

--- script/script.js
+++ script/script.js
@@ -1,2 +1,14 @@
-
-    
+function query() {
+    if (window.innerWidth >=768) {
+        menu.style.display = 'flex'
+    } else {
+        menu.style.display = 'none'
+    } 
+}
+function clickmenu() {
+    if (menu.style.display == 'block') {
+        menu.style.display = 'none'
+    } else {
+        menu.style.display = 'block'
+    }
+}
